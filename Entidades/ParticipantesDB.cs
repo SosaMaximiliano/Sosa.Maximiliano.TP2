ab4ffad65078889f96101fbc0d18bd0f57ac7f91
Keppler515 <maxisosa25@hotmail.com>
2022-11-18 17:13:20 -0300

    Exceptions, Interfaces, Generics, Serializadora

--- Entidades/ParticipantesDB.cs
+++ Entidades/ParticipantesDB.cs
@@ -12,42 +12,49 @@ namespace Entidades
         public List<Jugador> ObtenerJugadores()
         {
             List<Jugador> participantes = new List<Jugador>();
-
-            //Guardo la conexion en una variable
-            //Creo un objeto de conexion y le paso la direccion por parametro
-            string connectionString = "Server = .; Database = SegundoTP; Trusted_Connection = True;Encrypt=False;";
-            SqlConnection connection = new SqlConnection(connectionString);
-
-            //Puente entre el programa y la DB
-            SqlCommand command = new SqlCommand();
-
-            //Abro la conexion
-            connection.Open();
-
-            //Le asigno el objeto SqlConnection
-            command.Connection = connection;
-
-            //Le digo que va a recibir la query como texto
-            command.CommandType = System.Data.CommandType.Text;
-
-
-            command.CommandText = "SELECT * FROM Participantes2";
-
-            SqlDataReader reader = command.ExecuteReader();
-
-
-            while (reader.Read())
+            
+            try
             {
-                string nombre = reader.GetString(0);
+                //Guardo la conexion en una variable
+                //Creo un objeto de conexion y le paso la direccion por parametro
+                string connectionString = "Server = .; Database = SegundoTP; Trusted_Connection = True;Encrypt=False;";
+                SqlConnection connection = new SqlConnection(connectionString);
 
-                participantes.Add(new Jugador(nombre));
+                //Puente entre el programa y la DB
+                SqlCommand command = new SqlCommand();
+
+                //Abro la conexion
+                connection.Open();
+
+                //Le asigno el objeto SqlConnection
+                command.Connection = connection;
+
+                //Le digo que va a recibir la query como texto
+                command.CommandType = System.Data.CommandType.Text;
+
+
+                command.CommandText = "SELECT * FROM Participantes2";
+
+                SqlDataReader reader = command.ExecuteReader();
+
+
+                while (reader.Read())
+                {
+                    string nombre = reader.GetString(0);
+
+                    participantes.Add(new Jugador(nombre));
+                }
+
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+
             }
-
-            if (connection.State == System.Data.ConnectionState.Open)
+            catch (Exception ex)
             {
-                connection.Close();
+                throw;
             }
-
             return participantes;
         }
     }
